6fb78ca1c4f6ff3af9d83531f4ff0a73b6494e23
Thomas Münch <tmuench@awi.de>
2020-01-16 14:33:58 +0100

    add package installation file.

---
+++ lib/dependencies.R
@@ -0,0 +1,14 @@
+##
+## aim:
+## install all package dependencies for "optimalcores".
+## relation:
+## https://github.com/EarthSystemDiagnostics/optimalcores
+##
+
+install.packages("abind")
+install.packages("arrangements")
+install.packages("RColorBrewer")
+
+devtools::install_github("EarthSystemDiagnostics/ecustools")
+devtools::install_github("EarthSystemDiagnostics/pfields")
+
