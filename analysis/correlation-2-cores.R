0715b91669d99213f8e09a7faf0e7a07e15936e4
Thomas Münch <tmuench@awi.de>
2020-01-28 20:46:14 +0100

    add analysis script for two-core sampling.

---
+++ analysis/correlation-2-cores.R
@@ -0,0 +1,115 @@
+#!/usr/bin/env Rscript
+
+##
+## aim:
+## script to analyse the correlation structure with temperature for averaging
+## two isotope cores assessed with expectation value across ring combinations.
+## relation:
+## https://github.com/EarthSystemDiagnostics/optimalcores;
+## Muench, Werner and Laepple (2019), in prep.
+##
+
+# make script to also run on the terminal
+if (interactive()) {
+
+  # interactive usage
+  source("init.R")
+
+} else {
+
+  # script usage
+  pwd <- Sys.getenv()[["PWD"]]
+  setwd(pwd)
+  source("../setup.R")
+  source("init.R")
+}
+
+# save the correlation data?
+SAVE <- TRUE
+
+# ------------------------------------------------------------------------------
+# Select data and define region of target sites
+
+model  <- selectData()
+dml    <- setTargetRegion(field = model$lnd.t2m, verbose = FALSE)
+
+# ------------------------------------------------------------------------------
+# Expected correlation depending on distance for averaging two cores
+
+cat("\n")
+cat(as.character(Sys.time()), "\n")
+cat("Running t2m...\n")
+
+t2m <- dml %>%
+  analyseTargetRegion(target.field = model$t2m,
+                      study.field = model$lnd.t2m, N = 2) %>%
+  processRegionalMean() %>%
+  data2matrix()
+
+cat("Running t2m.pw...\n")
+
+t2m.pw <- dml %>%
+  analyseTargetRegion(target.field = model$t2m,
+                      study.field = model$lnd.t2m.pw, N = 2) %>%
+  processRegionalMean() %>%
+  data2matrix()
+
+cat("Running oxy...\n")
+
+oxy <- dml %>%
+  analyseTargetRegion(target.field = model$t2m,
+                      study.field = model$lnd.oxy, N = 2) %>%
+  processRegionalMean() %>%
+  data2matrix()
+
+cat("Running oxy.pw...\n")
+
+oxy.pw <- dml %>%
+  analyseTargetRegion(target.field = model$t2m,
+                      study.field = model$lnd.oxy.pw, N = 2) %>%
+  processRegionalMean() %>%
+  data2matrix()
+
+if (SAVE) {
+
+  saved <- list(
+    t2m    = t2m,
+    t2m.pw = t2m.pw,
+    oxy    = oxy,
+    oxy.pw = oxy.pw
+  )
+  attr(saved, "version") <- Sys.Date()
+
+  saveRDS(saved, file = "analysis/ring-correlation_N=2.rds")
+
+}
+
+cat(as.character(Sys.time()), "\n")
+cat("done.\n")
+
+# ------------------------------------------------------------------------------
+# Plotting
+
+color.palette <- colorRampPalette(rev(RColorBrewer::brewer.pal(10, "RdYlBu")))
+distances <- attr(t2m, "scale") - attr(t2m, "scale")[1]
+
+Quartz(dpi = 300, file = file.path(
+  SAVEPATH, "main", "echam5_mpiom_wiso_two_core_correlation_01_t2m.png"))
+plotCorrelationContours(t2m, distances, color.palette, zlim = c(0.2, 1))
+dev.off()
+
+Quartz(dpi = 300, file = file.path(
+  SAVEPATH, "main", "echam5_mpiom_wiso_two_core_correlation_02_t2m.pw.png"))
+plotCorrelationContours(t2m.pw, distances, color.palette, zlim = c(0.2, 0.55))
+dev.off()
+
+Quartz(dpi = 300, file = file.path(
+  SAVEPATH, "main", "echam5_mpiom_wiso_two_core_correlation_03_oxy.png"))
+plotCorrelationContours(oxy, distances, color.palette, zlim = c(0.2, 0.5))
+dev.off()
+
+Quartz(dpi = 300, file = file.path(
+  SAVEPATH, "main", "echam5_mpiom_wiso_two_core_correlation_04_oxy.pw.png"))
+plotCorrelationContours(oxy.pw, distances, color.palette, zlim = c(0.15, 0.35))
+dev.off()
+
