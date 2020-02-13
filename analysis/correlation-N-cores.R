5551de6f44745292f4872d75a23f9233da57b7a4
Thomas Münch <tmuench@awi.de>
2020-02-13 14:34:07 +0100

    update analysis script for sampling N cores.

--- analysis/correlation-N-cores.R
+++ analysis/correlation-N-cores.R
@@ -25,110 +25,84 @@ if (interactive()) {
   source("init.R")
 }
 
+library(parallel)
+
 # save the correlation data?
 SAVE <- TRUE
 
 # ------------------------------------------------------------------------------
-# Select data and define region of target sites
+# Select data and set paramters
 
 model  <- selectData()
-dml    <- setTargetRegion(field = model$lnd.t2m, verbose = FALSE)
+field  <- model$lnd.oxy.pw
+
+n.optim <- 5
+nmc     <- 10^5
+
+mc.cores <- ceiling(0.5 * parallel::detectCores())
 
 # ------------------------------------------------------------------------------
-# Preliminary code to run sampling for EDML and Vostok sites with N = 3, 5
+# Run sampling for EDML site with N = 3, 5
+
+cat("\n")
+cat(as.character(Sys.time()), "\n")
+cat("Running EDML...\n")
 
 target <- setTarget(model$t2m, site = "edml")
-n.optim <- 5
 
 system.time(
-  edml.n3 <- sampleNFromRings(N = 3, nmc = 1000,
-                              field = model$lnd.oxy.pw,
+  edml.n3 <- sampleNFromRings(N = 3, nmc = nmc, field = field,
                               target = target$dat,
-                              distance.field = target$dist) %>%
+                              distance.field = target$dist,
+                              mc.cores = mc.cores) %>%
     processCores(n.optim = n.optim)
 )
 
 system.time(
-  edml.n5 <- sampleNFromRings(N = 5, nmc = 100,
-                              field = model$lnd.oxy.pw,
+  edml.n5 <- sampleNFromRings(N = 5, nmc = nmc, field = field,
                               target = target$dat,
-                              distance.field = target$dist) %>%
+                              distance.field = target$dist,
+                              mc.cores = mc.cores) %>%
     processCores(n.optim = n.optim)
 )
+
+# ------------------------------------------------------------------------------
+# Run sampling for Vostok site with N = 3, 5
+
+cat("Running Vostok...\n")
 
 target <- setTarget(model$t2m, site = "vostok")
 
 system.time(
-  vostok.n3 <- sampleNFromRings(N = 3, nmc = 1000,
-                                field = model$lnd.oxy.pw,
-                                target = target$dat,
-                                distance.field = target$dist) %>%
+  vost.n3 <- sampleNFromRings(N = 3, nmc = nmc, field = field,
+                              target = target$dat,
+                              distance.field = target$dist,
+                              mc.cores = mc.cores) %>%
     processCores(n.optim = n.optim)
 )
 
 system.time(
-  vostok.n5 <- sampleNFromRings(N = 5, nmc = 100,
-                                field = model$lnd.oxy.pw,
-                                target = target$dat,
-                                distance.field = target$dist) %>%
+  vost.n5 <- sampleNFromRings(N = 5, nmc = nmc, field = field,
+                              target = target$dat,
+                              distance.field = target$dist,
+                              mc.cores = mc.cores) %>%
     processCores(n.optim = n.optim)
 )
 
 # ------------------------------------------------------------------------------
-# Example code to run analysis across a region
+# Save output
 
-system.time(
-  test <- analyseTargetRegion(region = dml[1 : 3, ],
-                              target.field = model$t2m,
-                              study.field = model$lnd.t2m,
-                              N = 3, nmc = 100,
-                              verbose = TRUE) %>%
-    processRegionalMean() %>%
-    getOptimalCorrelations(n.optim = n.optim)
-)
+if (SAVE) {
 
-# ------------------------------------------------------------------------------
-# Plot ring occurrences
+  saved <- list(
+    edml = list(N3 = edml.n3, N5 = edml.n5),
+    vost = list(N3 = vost.n3, N5 = vost.n5)
+  )
+  attr(saved, "version") <- Sys.Date()
 
-adj  <- 0.985
-padj <- 0.7
+  saveRDS(saved, file = "analysis/ring_occurrences_edml_vostok_N=3_N=5.rds")
 
-Quartz(width = 14, height = 6,
-       file = file.path(SAVEPATH, "main", "echam5_mpiom_wiso_binning.pdf"))
-op <- par(LoadGraphicsPar(mfcol = c(2, 2),
-                          mar = c(0, 0, 0, 0),
-                          oma = c(5, 7.5, 2.5, 0.5)))
+}
 
-plotRingOccurrences(edml.n5, xlab = "", xaxt = "n", ylab = "")
-abline(h = 0.5, v = 2250,lwd = 5)
-mtext("(a)", side = 3, line = -1, las = 0, adj = adj, padj = padj,
-      cex = par()$cex.lab, font = 2)
-
-plotRingOccurrences(edml.n3, ylab = "")
-abline(v = 2250,lwd = 5)
-mtext("(c)", side = 3, line = -1, las = 0, adj = adj, padj = padj,
-      cex = par()$cex.lab, font = 2)
-
-plotRingOccurrences(vostok.n5, xlab = "", ylab = "", xaxt = "n", yaxt = "n")
-abline(h = 0.5,lwd = 5)
-mtext("(b)", side = 3, line = -1, las = 0, adj = adj, padj = padj,
-      cex = par()$cex.lab, font = 2)
-
-plotRingOccurrences(vostok.n3, ylab = "", yaxt = "n")
-mtext("(d)", side = 3, line = -1, las = 0, adj = adj, padj = padj,
-      cex = par()$cex.lab, font = 2)
-
-mtext("Rank", side = 2, line = 3.5, cex = par()$cex.lab, las = 0,
-      outer = TRUE, at = 0.5)
-mtext("N = 3", side = 2, line = 5.5, cex = par()$cex.lab, las = 0,
-      outer = TRUE, at = 0.25)
-mtext("N = 5", side = 2, line = 5.5, cex = par()$cex.lab, las = 0,
-      outer = TRUE, at = 0.75)
-
-mtext("DML", side = 3, line = 0.25, cex = par()$cex.lab,
-      outer = TRUE, at = 0.03)
-mtext("Vostok", side = 3, line = 0.25, cex = par()$cex.lab,
-      outer = TRUE, at = 0.54)
-
-dev.off()
-par(op)
+cat(as.character(Sys.time()), "\n")
+cat("done.\n")
