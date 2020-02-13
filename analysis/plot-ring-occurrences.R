bc86ff02f53dbf17118afc39ac277fbb5988a59c
Thomas Münch <tmuench@awi.de>
2020-02-13 14:34:41 +0100

    add script to plot ring occurrences.

---
+++ analysis/plot-ring-occurrences.R
@@ -0,0 +1,62 @@
+##
+## aim:
+## script to plot the occurrences of sampled rings, i.e. the number of cores put
+## into each ring; for different N and different target sites.
+##
+## relation:
+## https://github.com/EarthSystemDiagnostics/optimalcores;
+## Muench, Werner and Laepple (2019), in prep.
+##
+
+source("init.R")
+
+# ------------------------------------------------------------------------------
+# Settings and load data
+
+adj  <- 0.985
+padj <- 0.7
+
+dat <- readRDS("analysis/ring_occurrences_edml_vostok_N=3_N=5.rds")
+
+# ------------------------------------------------------------------------------
+# Plotting
+
+Quartz(width = 14, height = 6,
+       file = file.path(SAVEPATH, "main", "echam5_mpiom_wiso_binning.pdf"))
+op <- par(LoadGraphicsPar(mfcol = c(2, 2),
+                          mar = c(0, 0, 0, 0),
+                          oma = c(5, 7.5, 2.5, 0.5)))
+
+plotRingOccurrences(dat$edml$N5, xlab = "", xaxt = "n", ylab = "")
+abline(h = 0.5, v = 2250,lwd = 5)
+mtext("(a)", side = 3, line = -1, las = 0, adj = adj, padj = padj,
+      cex = par()$cex.lab, font = 2)
+
+plotRingOccurrences(dat$edml$N3, ylab = "")
+abline(v = 2250,lwd = 5)
+mtext("(c)", side = 3, line = -1, las = 0, adj = adj, padj = padj,
+      cex = par()$cex.lab, font = 2)
+
+plotRingOccurrences(dat$vost$N5, xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+abline(h = 0.5,lwd = 5)
+mtext("(b)", side = 3, line = -1, las = 0, adj = adj, padj = padj,
+      cex = par()$cex.lab, font = 2)
+
+plotRingOccurrences(dat$vost$N3, ylab = "", yaxt = "n")
+mtext("(d)", side = 3, line = -1, las = 0, adj = adj, padj = padj,
+      cex = par()$cex.lab, font = 2)
+
+mtext("Rank", side = 2, line = 3.5, cex = par()$cex.lab, las = 0,
+      outer = TRUE, at = 0.5)
+mtext("N = 3", side = 2, line = 5.5, cex = par()$cex.lab, las = 0,
+      outer = TRUE, at = 0.25)
+mtext("N = 5", side = 2, line = 5.5, cex = par()$cex.lab, las = 0,
+      outer = TRUE, at = 0.75)
+
+mtext("DML", side = 3, line = 0.25, cex = par()$cex.lab,
+      outer = TRUE, at = 0.03)
+mtext("Vostok", side = 3, line = 0.25, cex = par()$cex.lab,
+      outer = TRUE, at = 0.54)
+
+dev.off()
+par(op)
