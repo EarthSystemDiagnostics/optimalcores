6510a0d53a4f1688f0dbdbbcda1912a2227daff6
Thomas Münch <tmuench@awi.de>
2020-02-19 18:04:54 +0100

    produce combined plot for DML and Vostok regions.

--- analysis/correlation-versus-distance.R
+++ analysis/correlation-versus-distance.R
@@ -12,8 +12,14 @@ source("init.R")
 # ------------------------------------------------------------------------------
 # Select data and define region of target sites
 
-model  <- selectData()
-dml    <- setTargetRegion(field = model$lnd.t2m)
+model <- selectData()
+
+dml.region <- setTargetRegion(field = model$lnd.t2m)
+vos.region <- setTargetRegion(field = model$lnd.t2m,
+                              min.lat = -83.5, max.lat = -73.5,
+                              min.lon = 89.5, max.lon = 124.5)
+
+target.field <- model$t2m
 
 # ------------------------------------------------------------------------------
 # Example of sampling 1 : 3 cores for a single target site
@@ -35,21 +41,42 @@ triples <- sampleNFromRings(N = 3, field = model$lnd.t2m, target = target$dat,
 # ------------------------------------------------------------------------------
 # Determine DML correlation structure for t2m, t2m.pw, oxy and oxy.pw
 
-target.field <- model$t2m
+dml <- list()
 
-t2m <- dml %>%
+dml$t2m <- dml.region %>%
   analyseTargetRegion(target.field, study.field = model$lnd.t2m) %>%
   processRegionalMean()
 
-t2m.pw <- dml %>%
+dml$t2m.pw <- dml.region %>%
   analyseTargetRegion(target.field, study.field = model$lnd.t2m.pw) %>%
   processRegionalMean()
 
-oxy <- dml %>%
+dml$oxy <- dml.region %>%
   analyseTargetRegion(target.field, study.field = model$lnd.oxy) %>%
   processRegionalMean()
 
-oxy.pw <- dml %>%
+dml$oxy.pw <- dml.region %>%
+  analyseTargetRegion(target.field, study.field = model$lnd.oxy.pw) %>%
+  processRegionalMean()
+
+# ------------------------------------------------------------------------------
+# Determine Vostok region correlation structure for t2m, t2m.pw, oxy and oxy.pw
+
+vos <- list()
+
+vos$t2m <- vos.region %>%
+  analyseTargetRegion(target.field, study.field = model$lnd.t2m) %>%
+  processRegionalMean()
+
+vos$t2m.pw <- vos.region %>%
+  analyseTargetRegion(target.field, study.field = model$lnd.t2m.pw) %>%
+  processRegionalMean()
+
+vos$oxy <- vos.region %>%
+  analyseTargetRegion(target.field, study.field = model$lnd.oxy) %>%
+  processRegionalMean()
+
+vos$oxy.pw <- vos.region %>%
   analyseTargetRegion(target.field, study.field = model$lnd.oxy.pw) %>%
   processRegionalMean()
 
@@ -64,21 +91,44 @@ col1 <- "black"
 col2 <- "green4"
 col3 <- "dodgerblue3"
 
-Quartz(file.path(SAVEPATH, "main", "fig_03.pdf"))
-par(LoadGraphicsPar())
+label1 <- expression(bold("(a) ") * "DML")
+label2 <- expression(bold("(b) ") * "Vostok")
 
-plot(t2m$bins, t2m$samples$cor, type = "n",
+Quartz(file.path(SAVEPATH, "main", "fig_03.pdf"),
+       height = 5.5, width = 12)
+op <- par(LoadGraphicsPar(mfcol = c(1, 2),
+                          mar = c(0, 0, 0, 0),
+                          oma = c(5, 5, 2, 0.5)))
+
+plot(dml$t2m$distances$mean, dml$t2m$correlation$mean, type = "n",
      xlab = "", ylab = "",
      xlim = c(0, 2125), ylim = c(0, 1))
 mtext("Distance (km)", side = 1, line = 3.5, cex = par()$cex.lab)
 mtext("Correlation", side = 2, line = 3.5, cex = par()$cex.lab, las = 0)
+mtext(label1, side = 3, line = 1.1, cex = par()$cex.lab, adj = 0.02, padj = 1)
 
-lines(t2m$bins, t2m$samples$cor, col = col1, type = "b", lwd = 2.5)
-lines(oxy$bins, oxy$samples$cor, col = col2, type = "b", lwd = 2.5)
-lines(oxy.pw$bins, oxy.pw$samples$cor, col = col3, type = "b", lwd = 2.5)
+lines(dml$t2m$distances$mean, dml$t2m$correlation$mean,
+      col = col1, type = "b", lwd = 2.5)
+lines(dml$oxy$distances$mean, dml$oxy$correlation$mean,
+      col = col2, type = "b", lwd = 2.5)
+lines(dml$oxy.pw$distances$mean, dml$oxy.pw$correlation$mean,
+      col = col3, type = "b", lwd = 2.5)
+
+plot(vos$t2m$distances$mean, vos$t2m$correlation$mean, type = "n",
+     xlab = "", ylab = "", yaxt = "n",
+     xlim = c(0, 2125), ylim = c(0, 1))
+mtext("Distance (km)", side = 1, line = 3.5, cex = par()$cex.lab)
+mtext(label2, side = 3, line = 1.1, cex = par()$cex.lab, adj = 0.02, padj = 1)
+
+lines(vos$t2m$distances$mean, vos$t2m$correlation$mean,
+      col = col1, type = "b", lwd = 2.5)
+lines(vos$oxy$distances$mean, vos$oxy$correlation$mean,
+      col = col2, type = "b", lwd = 2.5)
+lines(vos$oxy.pw$distances$mean, vos$oxy.pw$correlation$mean,
+      col = col3, type = "b", lwd = 2.5)
 
 legend("topright", label, col = c(col1, col2, col3),
        lty = 1, lwd = 2.5, bty = "n")
 
 dev.off()
-
+par(op)
