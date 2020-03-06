3c60da2d7a4ba17f293caccb78f0ee165dd000be
Thomas Münch <tmuench@awi.de>
2020-03-06 15:22:56 +0100

    estimate conceptual model params and plot model.

---
+++ analysis/estimate-model-params.R
@@ -0,0 +1,133 @@
+##
+## aim:
+## script to estimate parameters of the coceptual model.
+## relation:
+## https://github.com/EarthSystemDiagnostics/optimalcores;
+## Muench, Werner and Laepple (2019), in prep.
+##
+
+source("init.R")
+
+# ------------------------------------------------------------------------------
+# Select data and define region of target sites
+
+model <- selectData()
+
+dml.region <- setTargetRegion(field = model$lnd.t2m)
+vos.region <- setTargetRegion(field = model$lnd.t2m,
+                              min.lat = -83.5, max.lat = -73.5,
+                              min.lon = 89.5, max.lon = 124.5)
+
+# ------------------------------------------------------------------------------
+# decorrelation lengths
+
+# t2m
+
+target.field <- model$t2m
+
+dml.t2m <- dml.region %>%
+  analyseTargetRegion(target.field, study.field = model$lnd.t2m) %>%
+  processRegionalMean()
+
+vos.t2m <- vos.region %>%
+  analyseTargetRegion(target.field, study.field = model$lnd.t2m) %>%
+  processRegionalMean()
+
+m1 <- lm(log(dml.t2m$correlation$mean) ~ dml.t2m$distances$mean + 0)
+m2 <- lm(log(vos.t2m$correlation$mean) ~ vos.t2m$distances$mean + 0)
+
+tau.dml.t2m <- -1 / coefficients(m1)
+tau.vos.t2m <- -1 / coefficients(m2)
+
+# t2m.pw
+
+dml.t2m.pw <- dml.region %>%
+  analyseTargetRegion(target.field, study.field = model$lnd.t2m.pw) %>%
+  processRegionalMean()
+
+m3 <- lm(log(dml.t2m.pw$correlation$mean) ~ dml.t2m.pw$distances$mean)# + 0)
+
+tau.dml.t2m.pw <- -1 / coefficients(m3)[2]
+xi.dml.t2m.pw   <- 1 - exp(2 * coefficients(m3)[1])
+
+# oxy.pw
+
+target.field <- model$oxy
+
+dml.oxy <- dml.region %>%
+  analyseTargetRegion(target.field, study.field = model$lnd.oxy) %>%
+  processRegionalMean()
+
+vos.oxy <- vos.region %>%
+  analyseTargetRegion(target.field, study.field = model$lnd.oxy) %>%
+  processRegionalMean()
+
+m4 <- lm(log(dml.oxy$correlation$mean) ~ dml.oxy$distances$mean + 0)
+m5 <- lm(log(vos.oxy$correlation$mean) ~ vos.oxy$distances$mean + 0)
+
+tau.dml.oxy <- -1 / coefficients(m4)
+tau.vos.oxy <- -1 / coefficients(m5)
+
+# ------------------------------------------------------------------------------
+# xi parameter from local t2m-t2m.pw correlation
+
+t2m.vs.t2m.pw.cor <- pfields::ApplyFields(model$t2m, model$t2m.pw, cor)
+avg.correlation   <- rowMeans(t2m.vs.t2m.pw.cor, na.rm = TRUE)
+
+dml.region <- setTargetRegion(field = t2m.vs.t2m.pw.cor)
+vos.region <- setTargetRegion(field = t2m.vs.t2m.pw.cor,
+                              min.lat = -83.5, max.lat = -73.5,
+                              min.lon = 89.5, max.lon = 124.5)
+
+avg.correlation.dml <- rowMeans(t2m.vs.t2m.pw.cor[, dml.region$field.indices],
+                                na.rm = TRUE)
+avg.correlation.vos <- rowMeans(t2m.vs.t2m.pw.cor[, vos.region$field.indices])
+
+# xi parameter averaged across Antarctica
+xi <- 1 - (avg.correlation)^2
+
+# xi parameter averaged across DML and Vostok region, respectively
+xi.dml <- 1 - (avg.correlation.dml)^2
+xi.vos <- 1 - (avg.correlation.vos)^2
+
+# ------------------------------------------------------------------------------
+# plot comparison of DML temperature decorrelation lengths
+
+x <- seq(min(dml.t2m$distances$mean), max(dml.t2m$distances$mean), 1)
+
+col1 <- "black"
+col2 <- "darkgrey"
+col3 <- "green4"
+col4 <- "dodgerblue3"
+
+label <- c(expression(italic("T")["2m"]^{"(pw)"}),
+           "independent fit",
+           "parameter curve",
+           expression(italic("T")["2m"] * " with fit"))
+
+Quartz(file.path(SAVEPATH, "main", "fig_B01.pdf"))
+op <- par(LoadGraphicsPar())
+
+plot(dml.t2m$distances$mean, dml.t2m$correlation$mean, type = "n",
+     xlab = "", ylab = "",
+     xlim = c(0, 2125), ylim = c(0, 1))
+mtext("Distance (km)", side = 1, line = 3.5, cex = par()$cex.lab)
+mtext("Correlation", side = 2, line = 3.5, cex = par()$cex.lab, las = 0)
+
+lines(x, exp(-x/tau.dml.t2m), lty = 2, lwd = 1.5, col = col2)
+
+lines(x, sqrt(1 - xi.dml.t2m.pw) * exp(-x/tau.dml.t2m.pw),
+      lty = 2, lwd = 1.5, col = col3)
+lines(x, sqrt(1 - xi.dml) * exp(-x/tau.dml.t2m),
+      lty = 2, lwd = 1.5, col = col4)
+
+lines(dml.t2m$distances$mean, dml.t2m$correlation$mean,
+      col = col2, type = "b", lwd = 2.5)
+lines(dml.t2m.pw$distances$mean, dml.t2m.pw$correlation$mean,
+      col = col1, type = "b", lwd = 2.5)
+
+legend("topright", label, col = c(col1, col3, col4, col2),
+       lty = c(1, 2, 2, 1), lwd = c(2.5, 1.5, 1.5, 2), bty = "n")
+
+par(op)
+dev.off()
---
+++ analysis/plot-conceptual-model.R
@@ -0,0 +1,80 @@
+##
+## aim:
+## script to plot conceptual model results.
+## relation:
+## https://github.com/EarthSystemDiagnostics/optimalcores;
+## Muench, Werner and Laepple (2019), in prep.
+##
+
+source("init.R")
+
+# ------------------------------------------------------------------------------
+# Model parameters
+
+# ring radii
+r <- seq(0, 2000, 10)
+
+# t2m decorrelation length (DML/Vostok region estimate)
+tau <- 1900
+
+# oxy decorrelation length (DML/Vostok region estimate)
+tau.d <- 1100
+
+# decorrelation length of precipitation intermittency effect (unconstrained)
+tau.pw <- 500
+
+# intermittency parameter (DML region estimate)
+xi <- 0.73
+
+# t2m-oxy correlation parameters (simple linear decay with distance)
+c0 <- 0.4
+c1 <- 0.2
+d0 <- 1500
+
+# ------------------------------------------------------------------------------
+# Run model for the various fields
+
+t2m    <- runConceptualModel(r, tau = tau, tau.d = tau.d, tau.pw = tau.pw,
+                             xi = xi, c0 = c0, c1 = c1, d0 = d0,
+                             field = "t2m")
+
+t2m.pw <- runConceptualModel(r, tau = tau, tau.d = tau.d, tau.pw = tau.pw,
+                             xi = xi, c0 = c0, c1 = c1, d0 = d0,
+                             field = "t2m.pw")
+
+oxy.pw <- runConceptualModel(r, tau = tau, tau.d = tau.d, tau.pw = tau.pw,
+                             xi = xi, c0 = c0, c1 = c1, d0 = d0,
+                             field = "oxy.pw")
+
+# ------------------------------------------------------------------------------
+# Plot model results
+
+color.palette <- colorRampPalette(rev(RColorBrewer::brewer.pal(10, "RdYlBu")))
+distances <- r
+label <- c(expression(bold("(a) ") * italic("T")["2m"]),
+           expression(bold("(b) ") * italic("T")["2m"]^{"(pw)"}),
+           expression(bold("(c) ") * delta^{18} * "O"^{"(pw)"}))
+
+filebase <- "conceptual_model_correlation_"
+
+Quartz(dpi = 300, file = file.path(
+  SAVEPATH, "main", paste0(filebase, "01_t2m.png")))
+plotCorrelationContours(t2m, distances, color.palette, zlim = c(0.3, 1),
+                        xlab.pos = seq(0, 2000, 500), label = label[1],
+                        dx = seq(250, 1750, 500))
+dev.off()
+
+Quartz(dpi = 300, file = file.path(
+  SAVEPATH, "main", paste0(filebase, "02_t2m.pw.png")))
+plotCorrelationContours(t2m.pw, distances, color.palette, zlim = c(0.2, 0.6),
+                        xlab.pos = seq(0, 2000, 500), label = label[2],
+                        dx = seq(250, 1750, 500))
+dev.off()
+
+Quartz(dpi = 300, file = file.path(
+  SAVEPATH, "main", paste0(filebase, "03_oxy.pw.png")))
+plotCorrelationContours(oxy.pw, distances, color.palette, zlim = c(0.1, 0.25),
+                        xlab.pos = seq(0, 2000, 500), label = label[3],
+                        dx = seq(250, 1750, 500))
+dev.off()
+
