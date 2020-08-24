4506c71d0393e90b499bcd3d62d9f6e28940c9f6
Thomas Münch <tmuench@awi.de>
2020-08-24 11:16:46 +0200

    mark main study regions and target sites.

--- analysis/plot-concept-figure.R
+++ analysis/plot-concept-figure.R
@@ -30,6 +30,19 @@ for (i in 1 : length(radii)) {
 }
 rings <- do.call(rbind, df.lst)
 
+# Main study sites and regions
+edml <- data.frame(lon = 0, lat = -75)
+vost <- data.frame(lon = 106.83, lat = -78.47)
+
+edml.region <- data.frame(
+  lon = edml$lon + 17.5 * c(-1, 1, 1, -1),
+  lat = edml$lat + 5 * c(1, 1, -1, -1)
+)
+vost.region <- data.frame(
+  lon = vost$lon + 17.5 * c(-1, 1, 1, -1),
+  lat = vost$lat + 5 * c(1, 1, -1, -1)
+)
+
 # ------------------------------------------------------------------------------
 # Plotting
 
@@ -40,10 +53,18 @@ p <- ggplot()
 
 p <- p +
 
+  geom_point(data = edml, aes(x = lon, y = lat),
+             col = "black", bg = "grey", size = 2, pch = 24, stroke = 1) +
+  geom_point(data = vost, aes(x = lon, y = lat),
+             col = "black", bg = "grey", size = 2, pch = 25, stroke = 1) +
+  geom_polygon(data = edml.region, aes(x = lon, y = lat),
+               col = "black", fill = "transparent", size = 0.5) +
+  geom_polygon(data = vost.region, aes(x = lon, y = lat),
+               col = "black", fill = "transparent", size = 0.5) +
   geom_path(data = rings, aes(x = lon, y = lat, group = id),
-                     col = "red", size = 0.75) +
+            col = "red", size = 0.75) +
   geom_point(data = focus, aes(x = lon, y = lat),
-                      col = "black", size = 2.5, pch = 3, stroke = 1)
+             col = "black", size = 2.5, pch = 3, stroke = 1)
 
 p <- ecustools::ggpolar(pole = "S", max.lat = -60, min.lat = -90,
                         n.lat.labels = 3,
